105682ad5cf3c1982c0f26d7c604bd502ea36f1b
Julian Torres <juliantorresssr@gmail.com>
2025-09-14 17:33:20 -0300

    add: User attributes

--- common/src/main/java/com/grupo3/entity/User.java
+++ common/src/main/java/com/grupo3/entity/User.java
@@ -4,9 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
+
+  private String id;
+
+  private String name;
+
+  private String surname;
+
+  private String password;
+
+  private String email;
+
+  private LocalDateTime memberSince;
+
+  private LocalDateTime lastLogin;
+
+  private Set<Role> roles;
+
+  private Status status;
 
 }
