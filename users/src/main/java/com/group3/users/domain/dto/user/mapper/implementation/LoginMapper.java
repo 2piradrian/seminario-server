43b95c2d736dd94ef9ef96f76ccb6687409c770b
Julian Torres <juliantorresssr@gmail.com>
2025-09-20 16:13:47 -0300

    add: LoginMapper class

---
+++ users/src/main/java/com/group3/users/domain/dto/user/mapper/implementation/LoginMapper.java
@@ -0,0 +1,24 @@
+package com.group3.users.domain.dto.user.mapper.implementation;
+
+import com.group3.entity.Token;
+import com.group3.users.domain.dto.user.request.LoginUserReq;
+import com.group3.users.domain.dto.user.response.LoginUserRes;
+
+import java.util.Map;
+
+public class LoginMapper {
+
+  public LoginUserReq toRequest(Map<String, Object> payload) {
+    return LoginUserReq.create(
+      (String) payload.get("email"),
+      (String) payload.get("password")
+    );
+  }
+
+  public LoginUserRes toResponse(Token token) {
+    return new LoginUserRes(
+      token
+    );
+  }
+
+}
