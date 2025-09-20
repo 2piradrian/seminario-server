5f1633df686a2cf55da82748907f931298c4f19f
Julian Torres <juliantorresssr@gmail.com>
2025-09-20 19:34:58 -0300

    add: Style enum to common lib

---
+++ common/src/main/java/com/group3/entity/Style.java
@@ -0,0 +1,26 @@
+package com.group3.entity;
+
+public enum Style {
+
+  POP,
+  ROCK,
+  HIP_HOP,
+  ELECTRONIC,
+  COUNTRY,
+  REGGAE,
+  METAL,
+  JAZZ,
+  BLUES,
+  CLASSICAL,
+  LATIN_POP,
+  REGGAETON,
+  CUMBIA,
+  SALSA,
+  BACHATA,
+  MERENGUE,
+  TROPICAL,
+  SAMBA,
+  BOSSA_NOVA,
+  RANCHERA
+
+}
