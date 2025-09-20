45512de48951bbe8b4038291cf407c89470907ca
Julian Torres <juliantorresssr@gmail.com>
2025-09-20 12:33:35 -0300

    add: GetUserById response class

---
+++ users/src/main/java/com/group3/users/domain/dto/user/response/GetUserByIdRes.java
@@ -0,0 +1,28 @@
+package com.group3.users.domain.dto.user.response;
+
+import com.group3.entity.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Getter
+@AllArgsConstructor
+public class GetUserByIdRes {
+
+  private final String id;
+
+  private final String name;
+
+  private final String surname;
+
+  private final String email;
+
+  private final Set<Role> roles;
+
+  private LocalDateTime memberSince;
+
+  private LocalDateTime lastLogin;
+
+}
