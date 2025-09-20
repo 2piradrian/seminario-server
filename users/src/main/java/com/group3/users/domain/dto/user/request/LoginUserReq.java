e72e7868ec2771edde12517d644ea778a8e4de40
Julian Torres <juliantorresssr@gmail.com>
2025-09-20 15:04:14 -0300

    add: LoginUser request class

---
+++ users/src/main/java/com/group3/users/domain/dto/user/request/LoginUserReq.java
@@ -0,0 +1,43 @@
+package com.group3.users.domain.dto.user.request;
+
+import com.group3.error.ErrorHandler;
+import com.group3.error.ErrorType;
+import com.group3.users.domain.validator.RegexValidators;
+import lombok.Getter;
+
+@Getter
+public class LoginUserReq {
+
+  private final String email;
+
+  private final String password;
+
+  private LoginUserReq(String email, String password){
+    this.email = email;
+    this.password = password;
+  }
+
+  public  static  LoginUserReq create(String email, String password){
+
+    if (password == null || email == null){
+      throw new ErrorHandler(ErrorType.MISSING_REQUIRED_FIELDS);
+    }
+
+    if (password.isEmpty() || email.isEmpty()) {
+      throw new ErrorHandler(ErrorType.MISSING_REQUIRED_FIELDS);
+    }
+
+    RegexValidators passwordValidator = RegexValidators.PASSWORD;
+    if (!password.matches(passwordValidator.getRegex())){
+      throw new ErrorHandler(ErrorType.INVALID_FIELDS);
+    }
+
+    RegexValidators emailValidator = RegexValidators.EMAIL;
+    if (!email.matches(emailValidator.getRegex())){
+      throw new ErrorHandler(ErrorType.INVALID_FIELDS);
+    }
+
+    return new LoginUserReq(email,password);
+  }
+
+}
