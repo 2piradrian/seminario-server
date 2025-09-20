3fd2e20c1f016b0dde56f972f793adad53ec3a10
Julian Torres <juliantorresssr@gmail.com>
2025-09-20 18:16:41 -0300

    add: UserService interface

---
+++ users/src/main/java/com/group3/users/presentation/service/UserServiceI.java
@@ -0,0 +1,21 @@
+package com.group3.users.presentation.service;
+
+import com.group3.users.domain.dto.user.request.*;
+import com.group3.users.domain.dto.user.response.AuthUserRes;
+import com.group3.users.domain.dto.user.response.GetUserByIdRes;
+import com.group3.users.domain.dto.user.response.LoginUserRes;
+import com.group3.users.domain.dto.user.response.RegisterUserRes;
+
+public interface UserServiceI {
+
+  GetUserByIdRes getById(GetUserByIdReq dto);
+
+  RegisterUserRes register(RegisterUserReq dto);
+
+  LoginUserRes login(LoginUserReq dto);
+
+  AuthUserRes auth(AuthUserReq dto);
+
+  void delete(DeleteUserReq dto);
+
+}
