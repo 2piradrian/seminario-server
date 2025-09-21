c8412d4b9c049541b1efbe60031f762c7b73c5a6
Julian Torres <juliantorresssr@gmail.com>
2025-09-21 15:27:18 -0300

    fix: tabs in config

--- users/src/main/java/com/group3/users/config/helpers/JWTHelper.java
+++ users/src/main/java/com/group3/users/config/helpers/JWTHelper.java
@@ -16,53 +16,53 @@ import java.util.function.Function;
 @Component
 public class JWTHelper {
 
-  @Value("${application.jwt.secret}")
-  private String secret;
+    @Value("${application.jwt.secret}")
+    private String secret;
 
-  @Value("${application.jwt.expiration}")
-  private Long expiration;
+    @Value("${application.jwt.expiration}")
+    private Long expiration;
 
-  private SecretKey getSecretKey(){
-      return Keys.hmacShaKeyFor(this.secret.getBytes(StandardCharsets.UTF_8));
-  }
+    private SecretKey getSecretKey(){
+        return Keys.hmacShaKeyFor(this.secret.getBytes(StandardCharsets.UTF_8));
+    }
 
-  private Date getExpirationDate(String token) {
-    return this.getClaims(token, Claims::getExpiration);
-  }
+    private Date getExpirationDate(String token) {
+        return this.getClaims(token, Claims::getExpiration);
+    }
 
-  private <T> T getClaims(String token, Function<Claims, T> resolver) {
-    // gets the claims from the token and signs it
-    return resolver.apply(this.signToken(token));
-  }
+    private <T> T getClaims(String token, Function<Claims, T> resolver) {
+        // gets the claims from the token and signs it
+        return resolver.apply(this.signToken(token));
+    }
 
-  public String getSubject(String token) {
-    return this.getClaims(token, Claims::getSubject);
-  }
+    public String getSubject(String token) {
+        return this.getClaims(token, Claims::getSubject);
+    }
 
-  private Claims signToken(String token){
-    return Jwts.parserBuilder()
-      .setSigningKey(this.getSecretKey())
-      .build()
-      .parseClaimsJws(token)
-      .getBody();
-  }
+    private Claims signToken(String token){
+        return Jwts.parserBuilder()
+            .setSigningKey(this.getSecretKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+    }
 
-  public String createToken (User user) {
-    final var now = new Date();
-    final var expirationDate = new Date(now.getTime() + this.expiration);
+    public String createToken (User user) {
+        final var now = new Date();
+        final var expirationDate = new Date(now.getTime() + this.expiration);
 
-    return Jwts.builder()
-      .setSubject(user.getEmail())
-      .setIssuedAt(new Date(System.currentTimeMillis()))
-      .setExpiration(expirationDate)
-      .signWith(this.getSecretKey())
-      .compact();
-  }
+        return Jwts.builder()
+            .setSubject(user.getEmail())
+            .setIssuedAt(new Date(System.currentTimeMillis()))
+            .setExpiration(expirationDate)
+            .signWith(this.getSecretKey())
+            .compact();
+    }
 
-  public boolean validateToken(String token) {
-    final var expirationDate = this.getExpirationDate(token);
+    public boolean validateToken(String token) {
+        final var expirationDate = this.getExpirationDate(token);
 
-    return expirationDate.after(new Date());
-  }
+        return expirationDate.after(new Date());
+    }
 
 }
