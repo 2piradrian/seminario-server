7e294f4c39c2ea6bae48d566ee44a28e29f47847
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-09-21 18:14:50 -0300

    catalog services

---
+++ catalog/src/main/java/com/group3/catalog/presentation/service/InstrumentService.java
@@ -0,0 +1,26 @@
+package com.group3.catalog.presentation.service;
+
+import com.group3.catalog.domain.dto.instrument.request.GetInstrumentByIdReq;
+import com.group3.catalog.domain.dto.instrument.request.GetInstrumentListByIdReq;
+import com.group3.catalog.domain.dto.instrument.response.GetAllInstrumentRes;
+import com.group3.catalog.domain.dto.instrument.response.GetInstrumentByIdRes;
+import com.group3.catalog.domain.dto.instrument.response.GetInstrumentListByIdRes;
+
+public class InstrumentService implements InstrumentServiceI {
+
+    @Override
+    public GetAllInstrumentRes getAll() {
+        return null;
+    }
+
+    @Override
+    public GetInstrumentByIdRes getById(GetInstrumentByIdReq req) {
+        return null;
+    }
+
+    @Override
+    public GetInstrumentListByIdRes getListById(GetInstrumentListByIdReq req) {
+        return null;
+    }
+
+}
---
+++ catalog/src/main/java/com/group3/catalog/presentation/service/StyleService.java
@@ -0,0 +1,26 @@
+package com.group3.catalog.presentation.service;
+
+import com.group3.catalog.domain.dto.style.request.GetStyleByIdReq;
+import com.group3.catalog.domain.dto.style.request.GetStyleListByIdReq;
+import com.group3.catalog.domain.dto.style.response.GetAllStyleRes;
+import com.group3.catalog.domain.dto.style.response.GetStyleByIdRes;
+import com.group3.catalog.domain.dto.style.response.GetStyleListByIdRes;
+
+public class StyleService implements StyleServiceI {
+
+    @Override
+    public GetAllStyleRes getAll() {
+        return null;
+    }
+
+    @Override
+    public GetStyleByIdRes getById(GetStyleByIdReq req) {
+        return null;
+    }
+
+    @Override
+    public GetStyleListByIdRes getListById(GetStyleListByIdReq req) {
+        return null;
+    }
+
+}
