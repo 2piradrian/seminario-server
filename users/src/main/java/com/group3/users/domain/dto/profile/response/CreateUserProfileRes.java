ce2e0baf3163501e53068e924b55d3663f8f8dcf
Julian Torres <juliantorresssr@gmail.com>
2025-09-21 11:43:13 -0300

    add: CreteUserProfileRes

---
+++ users/src/main/java/com/group3/users/domain/dto/profile/response/CreateUserProfileRes.java
@@ -0,0 +1,28 @@
+package com.group3.users.domain.dto.profile.response;
+
+import com.group3.entity.Instrument;
+import com.group3.entity.Style;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.Set;
+
+@Getter
+@AllArgsConstructor
+public class CreateUserProfileRes {
+
+  private final String id;
+
+  private final String portaitImage;
+
+  private final String profileImage;
+
+  private final String shortDescription;
+
+  private final String longDescription;
+
+  private final Set<Style> styles;
+
+  private final Set<Instrument> instruments;
+  
+}
