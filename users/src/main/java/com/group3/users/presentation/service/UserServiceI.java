295327b2127ea82a34ca8b97216eb626d3a60991
Julian Torres <juliantorresssr@gmail.com>
2025-09-21 19:10:18 -0300

    remove: UserProfileService interface, add: add update method to UserService interface

--- users/src/main/java/com/group3/users/presentation/service/UserServiceI.java
+++ users/src/main/java/com/group3/users/presentation/service/UserServiceI.java
@@ -1,10 +1,7 @@
 package com.group3.users.presentation.service;
 
 import com.group3.users.domain.dto.user.request.*;
-import com.group3.users.domain.dto.user.response.AuthUserRes;
-import com.group3.users.domain.dto.user.response.GetUserByIdRes;
-import com.group3.users.domain.dto.user.response.LoginUserRes;
-import com.group3.users.domain.dto.user.response.RegisterUserRes;
+import com.group3.users.domain.dto.user.response.*;
 
 public interface UserServiceI {
 
@@ -15,6 +12,8 @@ public interface UserServiceI {
     LoginUserRes login(LoginUserReq dto);
 
     AuthUserRes auth(AuthUserReq dto);
+
+    EditUserRes update(EditUserReq dto);
 
     void delete(DeleteUserReq dto);
 
