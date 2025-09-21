3f75893fa86f4ce18379d1151f5f443b88612c86
Julian Torres <juliantorresssr@gmail.com>
2025-09-21 16:06:11 -0300

    add: lombok notation to Instrument

--- common/src/main/java/com/group3/entity/Instrument.java
+++ common/src/main/java/com/group3/entity/Instrument.java
@@ -1,5 +1,12 @@
 package com.group3.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Instrument {
 
     private String id;
