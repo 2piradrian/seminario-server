233ce15030a874f6370f93e9af380283bc0b94c2
Julian Torres <juliantorresssr@gmail.com>
2025-09-21 10:50:40 -0300

    add: UserProfileEntityMapper class

---
+++ users/src/main/java/com/group3/users/data/postgres/mapper/UserProfileEntityMapper.java
@@ -0,0 +1,33 @@
+package com.group3.users.data.postgres.mapper;
+
+
+import com.group3.entity.UserProfile;
+import com.group3.users.data.postgres.model.UserProfileModel;
+
+public class UserProfileEntityMapper {
+
+  public static UserProfile toDomain(UserProfileModel userProfileModel){
+    return new UserProfile(
+      userProfileModel.getId(),
+      userProfileModel.getPortaitImage(),
+      userProfileModel.getProfileImage(),
+      userProfileModel.getShortDescription(),
+      userProfileModel.getLongDescription(),
+      userProfileModel.getStyles(),
+      userProfileModel.getInstruments()
+    );
+  }
+
+  public static UserProfileModel toModel(UserProfile userProfile){
+    return new UserProfileModel(
+      userProfile.getId(),
+      userProfile.getPortaitImage(),
+      userProfile.getProfileImage(),
+      userProfile.getShortDescription(),
+      userProfile.getLongDescription(),
+      userProfile.getStyles(),
+      userProfile.getInstruments()
+    );
+  }
+
+}
