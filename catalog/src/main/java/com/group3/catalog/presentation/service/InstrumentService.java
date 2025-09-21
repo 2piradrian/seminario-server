122a3b630f6ccfed754a5a534e6b858fc3cf677e
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-09-21 18:58:30 -0300

    instrument service

--- catalog/src/main/java/com/group3/catalog/presentation/service/InstrumentService.java
+++ catalog/src/main/java/com/group3/catalog/presentation/service/InstrumentService.java
@@ -1,14 +1,22 @@
 package com.group3.catalog.presentation.service;
 
+import com.group3.catalog.data.repository.InstrumentRepository;
+import com.group3.catalog.domain.dto.instrument.mapper.InstrumentMapper;
 import com.group3.catalog.domain.dto.instrument.request.GetInstrumentByIdReq;
 import com.group3.catalog.domain.dto.instrument.request.GetInstrumentListByIdReq;
 import com.group3.catalog.domain.dto.instrument.response.GetAllInstrumentRes;
 import com.group3.catalog.domain.dto.instrument.response.GetInstrumentByIdRes;
 import com.group3.catalog.domain.dto.instrument.response.GetInstrumentListByIdRes;
+import com.group3.entity.Instrument;
+import com.group3.error.ErrorHandler;
+import com.group3.error.ErrorType;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @Service
@@ -16,19 +24,37 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class InstrumentService implements InstrumentServiceI {
 
+    private final InstrumentRepository repository;
+
     @Override
     public GetAllInstrumentRes getAll() {
-        return null;
+        List<Instrument> instruments = this.repository.getAll();
+        return InstrumentMapper.getAll().toResponse(instruments);
     }
 
     @Override
     public GetInstrumentByIdRes getById(GetInstrumentByIdReq dto) {
-        return null;
+        Instrument instrument = this.repository.getById(dto.getId());
+
+        if (instrument == null) {
+            throw new ErrorHandler(ErrorType.INSTRUMENT_NOT_FOUND);
+        }
+
+        return InstrumentMapper.getById().toResponse(instrument);
     }
 
     @Override
     public GetInstrumentListByIdRes getListById(GetInstrumentListByIdReq dto) {
-        return null;
+        List<Instrument> instruments = dto.getIds().stream()
+                .map(this.repository::getById)
+                .filter(Objects::nonNull)
+                .toList();
+
+        if (instruments.isEmpty()) {
+            throw new ErrorHandler(ErrorType.INSTRUMENT_NOT_FOUND);
+        }
+
+        return InstrumentMapper.getListById().toResponse(instruments);
     }
 
 }
