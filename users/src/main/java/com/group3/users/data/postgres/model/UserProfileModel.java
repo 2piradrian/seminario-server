ac00c0e5a5fc4e3d41b1c53c250efc127a317b11
Julian Torres <juliantorresssr@gmail.com>
2025-09-21 10:43:19 -0300

    add: UserProfileModel class

---
+++ users/src/main/java/com/group3/users/data/postgres/model/UserProfileModel.java
@@ -0,0 +1,36 @@
+package com.group3.users.data.postgres.model;
+
+import com.group3.entity.Instrument;
+import com.group3.entity.Style;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user-profiles")
+public class UserProfileModel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private String id;
+
+  private String portaitImage;
+
+  private String profileImage;
+
+  private String shortDescription;
+
+  private String longDescription;
+
+  @Enumerated(EnumType.STRING)
+  private Set<Style> styles;
+
+  @Enumerated(EnumType.STRING)
+  private Set<Instrument> instruments;
+}
