696c625dad8aa05ca09450d57826157c64225fe2
Julian Torres <juliantorresssr@gmail.com>
2025-09-21 12:21:59 -0300

    add: UserProfileMapper with only create method

---
+++ users/src/main/java/com/group3/users/domain/dto/profile/mapper/UserProfileMapper.java
@@ -0,0 +1,9 @@
+package com.group3.users.domain.dto.profile.mapper;
+
+import com.group3.users.domain.dto.profile.mapper.implementation.*;
+
+public class UserProfileMapper {
+
+  public static CreateMapper create(){ return new CreateMapper(); };
+
+}
