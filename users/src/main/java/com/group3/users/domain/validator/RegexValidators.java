18043d64a61a1ed2fbb82849cec1893ce6bb31bc
Julian Torres <juliantorresssr@gmail.com>
2025-09-21 11:21:11 -0300

    add: add more regex validator for descriptions of user profiles

--- users/src/main/java/com/group3/users/domain/validator/RegexValidators.java
+++ users/src/main/java/com/group3/users/domain/validator/RegexValidators.java
@@ -8,7 +8,9 @@ public enum RegexValidators {
   NAME("^[\\p{L}]+(?:[ '-][\\p{L}]+)*$"),
   SURNAME("^[\\p{L}]+(?:[ '-][\\p{L}]+)*$"),
   PASSWORD("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$"),
-  EMAIL("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$");
+  EMAIL("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"),
+  SHORT_DESCRIPTION("^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9.,!?()'\"\\-\\s]{10,50}$"),
+  LONG_DESCRIPTION("^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9.,!?()'\"\\-\\s]{10,150}$");
 
   private final String regex;
 
