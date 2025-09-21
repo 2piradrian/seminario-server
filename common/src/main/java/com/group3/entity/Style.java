4d4910837d03552ef76926e716321a4839c55112
Julian Torres <juliantorresssr@gmail.com>
2025-09-21 16:07:05 -0300

    remove: style enum, add: Style class

--- common/src/main/java/com/group3/entity/Style.java
+++ common/src/main/java/com/group3/entity/Style.java
@@ -1,27 +1,9 @@
 package com.group3.entity;
 
-public enum Style {
+public class Style {
 
-    POP,
-    ROCK,
-    HIP_HOP,
-    ELECTRONIC,
-    COUNTRY,
-    REGGAE,
-    METAL,
-    JAZZ,
-    BLUES,
-    CLASSICAL,
-    LATIN_POP,
-    REGGAETON,
-    CUMBIA,
-    SALSA,
-    BACHATA,
-    MERENGUE,
-    TROPICAL,
-    SAMBA,
-    BOSSA_NOVA,
-    RANCHERA,
-    NONE
+    private String id;
+
+    private String name;
 
 }
