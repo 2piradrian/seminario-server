a2e33ca91a446644500ab5f5014debfc8d15e6f8
Julian Torres <juliantorresssr@gmail.com>
2025-09-21 16:35:57 -0300

    fix: raname interface

--- users/src/main/java/com/group3/users/presentation/service/UserProfileServiceI.java
+++ users/src/main/java/com/group3/users/presentation/service/UserProfileServiceI.java
@@ -3,7 +3,7 @@ package com.group3.users.presentation.service;
 import com.group3.users.domain.dto.profile.request.EditUserProfileReq;
 import com.group3.users.domain.dto.profile.response.EditUserProfileRes;
 
-public interface ProfileServiceI {
+public interface UserProfileServiceI {
 
     EditUserProfileRes edit(EditUserProfileReq dto);
 
