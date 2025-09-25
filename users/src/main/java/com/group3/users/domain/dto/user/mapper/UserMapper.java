a1cb77bf3838f9f9b58ca1fc805a8387f77101d4
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-09-25 18:17:39 -0300

    user service

---
+++ users/src/main/java/com/group3/users/domain/dto/user/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.group3.users.domain.dto.user.mapper;
+
+import com.group3.users.domain.dto.user.mapper.implementation.*;
+
+public class UserMapper {
+
+    public static GetByIdMapper getById() {
+        return new GetByIdMapper();
+    }
+
+    public static DeleteMapper delete() {
+        return new DeleteMapper();
+    }
+
+}
