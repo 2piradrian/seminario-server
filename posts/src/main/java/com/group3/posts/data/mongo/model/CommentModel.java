0d4316b2e0a1aeec6bf0b582a403627ea32907a9
milagrso <milagrosberoncapra1@gmail.com>
2025-09-26 15:06:54 -0300

    add data model comments

---
+++ posts/src/main/java/com/group3/posts/data/mongo/model/CommentModel.java
@@ -0,0 +1,45 @@
+package com.group3.posts.data.mongo.model;
+
+import com.group3.entity.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "comments")
+public class CommentModel {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.UUID)
+    private String Id;
+
+    private  String authorId;
+
+    @Column(name = "post_id", nullable = false)
+    private String postId;
+
+    @ManyToOne
+    private CommentModel replyTo;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private Set<String> upvoters;
+
+    private  Set<String> downvoters;
+
+    private LocalDateTime createdAt;
+
+    private  LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+}
