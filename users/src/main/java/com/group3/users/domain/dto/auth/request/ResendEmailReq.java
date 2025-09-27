6581a4adb13b40c9ee539de0fba460c0aa2c43fb
Julian Torres <juliantorresssr@gmail.com>
2025-09-27 12:35:49 -0300

    add: ResendEmail request class

---
+++ users/src/main/java/com/group3/users/domain/dto/auth/request/ResendEmailReq.java
@@ -0,0 +1,27 @@
+package com.group3.users.domain.dto.auth.request;
+
+import com.group3.error.ErrorHandler;
+import com.group3.error.ErrorType;
+import lombok.Getter;
+
+@Getter
+public class ResendEmailReq {
+
+    private final String email;
+
+    private ResendEmailReq(String email){
+        this.email = email;
+    }
+
+    public static ResendEmailReq create(String email) {
+
+        if (email == null){
+            throw new ErrorHandler(ErrorType.MISSING_REQUIRED_FIELDS);
+        }
+
+        return new ResendEmailReq(email);
+
+    }
+
+
+}
