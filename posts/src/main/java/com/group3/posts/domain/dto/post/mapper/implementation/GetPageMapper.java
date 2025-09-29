051aea032d1d3c84d19786f99c3e328a78fcc9ee
milagrso <milagrosberoncapra1@gmail.com>
2025-09-29 00:22:31 -0300

    add get page mapper for post

---
+++ posts/src/main/java/com/group3/posts/domain/dto/post/mapper/implementation/GetPageMapper.java
@@ -0,0 +1,25 @@
+package com.group3.posts.domain.dto.post.mapper.implementation;
+
+import com.group3.entity.PageContent;
+import com.group3.entity.Post;
+import com.group3.posts.domain.dto.post.request.GetPostPageReq;
+import com.group3.posts.domain.dto.post.response.GetPostPageRes;
+import jakarta.persistence.criteria.CriteriaBuilder;
+
+public class GetPageMapper {
+
+    public GetPostPageReq toRequest(String category, Integer size, Integer page) {
+        return GetPostPageReq.create(
+                category,
+                size,
+                page
+        );
+    }
+
+    public GetPostPageRes toResponse(PageContent<Post> posts) {
+        return new GetPostPageRes(
+                posts.getContent(),
+                posts.getNextPage()
+        );
+    }
+}
