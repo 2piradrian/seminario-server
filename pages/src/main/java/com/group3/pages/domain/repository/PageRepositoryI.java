d1aca3f1aa999e6497b11d148488c873bca3d867
JulianTs0 <juliantorresssr@gmail.com>
2025-09-29 20:48:19 -0300

    add: PageRepository interface

---
+++ pages/src/main/java/com/group3/pages/domain/repository/PageRepositoryI.java
@@ -0,0 +1,19 @@
+package com.group3.pages.domain.repository;
+
+import com.group3.entity.Page;
+
+import java.util.List;
+
+public interface PageRepositoryI {
+    
+    Page getById(String pageId);
+    
+    Page getByEmail(String ownerId);
+
+    List<Page> findByName(String name);
+
+    Page save(Page page);
+
+    Page update(Page page);
+    
+}
