b9121886c9d5de65ab73681f1b7e3312bf080c47
milagrso <milagrosberoncapra1@gmail.com>
2025-09-28 22:45:39 -0300

    add: get post page res

---
+++ posts/src/main/java/com/group3/posts/domain/dto/post/response/GetPostPageRes.java
@@ -0,0 +1,16 @@
+package com.group3.posts.domain.dto.post.response;
+
+import com.group3.entity.Post;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class GetPostPageRes {
+
+    List<Post> posts;
+
+    Integer nextPage;
+}
