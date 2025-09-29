890e0e6b58c2f65b0e2178799e4433f619a01053
milagrso <milagrosberoncapra1@gmail.com>
2025-09-29 00:15:40 -0300

    add get by id mapper for post

---
+++ posts/src/main/java/com/group3/posts/domain/dto/post/mapper/implementation/GetByIdMapper.java
@@ -0,0 +1,30 @@
+package com.group3.posts.domain.dto.post.mapper.implementation;
+
+import com.group3.entity.Post;
+import com.group3.entity.User;
+import com.group3.posts.domain.dto.post.request.GetPostByIdReq;
+import com.group3.posts.domain.dto.post.response.GetPostByIdRes;
+
+public class GetByIdMapper {
+
+    public GetPostByIdReq toRequest(String postId) {
+        return GetPostByIdReq.create(
+                postId
+        );
+    }
+
+    public GetPostByIdRes toResponse(Post post, User author) {
+        return new GetPostByIdRes(
+                author.getId(),
+                author.getUsername(),
+                post.getId(),
+                post.getTitle(),
+                post.getContent(),
+                post.getViews(),
+                post.getUpvoters().size(),
+                post.getDownvoters().size(),
+                post.getCategory(),
+                post.getCreatedAt()
+        );
+    }
+}
