315ba20da3f7af0e5d52ca1e5dd50e68fb0921a9
milagrso <milagrosberoncapra1@gmail.com>
2025-09-29 00:34:05 -0300

    add: post mapper

---
+++ posts/src/main/java/com/group3/posts/domain/dto/post/mapper/PostMapper.java
@@ -0,0 +1,31 @@
+package com.group3.posts.domain.dto.post.mapper;
+
+import com.group3.posts.domain.dto.post.mapper.implementation.*;
+
+public class PostMapper {
+
+    public static CreateMapper create() { return new CreateMapper(); }
+
+    public static GetPageMapper getPage() { return new GetPageMapper(); }
+
+    public static GetMonthlyMapper getMonthly() {
+        return new GetMonthlyMapper();
+    }
+
+    public static DeleteMapper delete() {
+        return new DeleteMapper();
+    }
+
+    public static EditMapper edit() {
+        return new EditMapper();
+    }
+
+    public static GetByIdMapper getById() {
+        return new GetByIdMapper();
+    }
+
+    public static ToggleVotesMapper toggleVotes() {
+        return new ToggleVotesMapper();
+    }
+
+}
