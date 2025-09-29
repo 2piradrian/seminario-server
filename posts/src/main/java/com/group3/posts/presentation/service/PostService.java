cb788c8aef5022cee5fdbef29b7753336d14bf97
milagrso <milagrosberoncapra1@gmail.com>
2025-09-29 00:57:43 -0300

    add post service interface

---
+++ posts/src/main/java/com/group3/posts/presentation/service/PostService.java
@@ -0,0 +1,21 @@
+package com.group3.posts.presentation.service;
+
+import com.group3.posts.domain.dto.post.request.*;
+import com.group3.posts.domain.dto.post.response.*;
+
+public interface PostService {
+
+    GetPostByIdRes getById(GetPostByIdReq dto);
+
+    GetPostPageRes getPosts(GetPostPageReq dto);
+
+    GetMonthlyPostsRes getMonthlyPosts(GetMonthlyPostReq dto);
+
+    CreatePostRes create(CreatePostReq dto);
+
+    EditPostRes edit(EditPostReq dto);
+
+    void  toggleVotes(TogglePostVotesReq dto);
+
+    void delete(DeletePostReq dto);
+}
