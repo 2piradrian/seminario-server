08fa7b3614b59bf1826457c5daa4e52709e6c349
milagrso <milagrosberoncapra1@gmail.com>
2025-09-28 23:37:35 -0300

    add create mapper for post

---
+++ posts/src/main/java/com/group3/posts/domain/dto/post/mapper/implementation/CreateMapper.java
@@ -0,0 +1,25 @@
+package com.group3.posts.domain.dto.post.mapper.implementation;
+
+import com.group3.entity.Post;
+import com.group3.posts.domain.dto.post.request.CreatePostReq;
+import com.group3.posts.domain.dto.post.response.CreatePostRes;
+
+import java.util.Map;
+
+public class CreateMapper {
+
+    public CreatePostReq toRequest(String token, Map<String, Object> payload) {
+        return CreatePostReq.create(
+                token,
+                (String) payload.get("title"),
+                (String) payload.get("content"),
+                (String) payload.get("category")
+        );
+    }
+
+    public CreatePostRes toResponse(Post post) {
+        return new CreatePostRes(
+                post.getId()
+        );
+    }
+}
