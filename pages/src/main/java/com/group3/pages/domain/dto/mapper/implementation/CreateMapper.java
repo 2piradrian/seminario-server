4524d4d637f6a8c8a04ba9852a1124c412bb2af8
JulianTs0 <juliantorresssr@gmail.com>
2025-09-30 15:39:41 -0300

    fix: createMapper class

--- pages/src/main/java/com/group3/pages/domain/dto/mapper/implementation/CreateMapper.java
+++ pages/src/main/java/com/group3/pages/domain/dto/mapper/implementation/CreateMapper.java
@@ -9,8 +9,7 @@ public class CreateMapper {
     public CreatePageReq toRequest(String token, Map<String, Object> payload){
         return CreatePageReq.create(
             token,
-            (String) payload.get("name"),
-            (String) payload.get("base64Image")
+            (String) payload.get("name")
         );
     }
     
