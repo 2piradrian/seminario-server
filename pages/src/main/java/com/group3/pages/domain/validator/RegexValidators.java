4f4fb29c6d44d188ae780eb75bc5053fe4a28175
JulianTs0 <juliantorresssr@gmail.com>
2025-09-30 15:21:52 -0300

    add: new regex to regex validator enum

--- pages/src/main/java/com/group3/pages/domain/validator/RegexValidators.java
+++ pages/src/main/java/com/group3/pages/domain/validator/RegexValidators.java
@@ -5,7 +5,9 @@ import lombok.Getter;
 @Getter
 public enum RegexValidators {
     
-    NAME("^[\\p{L}]+(?:[ '-][\\p{L}]+)*$");
+    NAME("^[\\p{L}]+(?:[ '-][\\p{L}]+)*$"),
+    SHORT_DESCRIPTION("^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9.,!?'\"¡¿@%&$*+_:;\\-\\s]{1,50}$"),
+    LONG_DESCRIPTION("^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9.,!?'\"¡¿@%&$*+_:;\\-\\s]{1,150}$");
 
     private final String regex;
 
