25bfe8c90d86f97233d61bfebd5eea3dd2ef736e
JulianTs0 <juliantorresssr@gmail.com>
2025-09-30 09:19:46 -0300

    add: new mapper methods with lists

--- pages/src/main/java/com/group3/pages/data/postgres/mapper/PageEntityMapper.java
+++ pages/src/main/java/com/group3/pages/data/postgres/mapper/PageEntityMapper.java
@@ -3,6 +3,10 @@ package com.group3.pages.data.postgres.mapper;
 import com.group3.entity.Page;
 import com.group3.pages.data.postgres.model.PageModel;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class PageEntityMapper {
     
     public static Page toDomain(PageModel pageModel) {
@@ -24,5 +28,21 @@ public class PageEntityMapper {
             page.getMembers()
         );
     }
-    
+
+        public static List<Page> toDomain(List<PageModel> pageModels) {
+        if (pageModels == null) return Collections.emptyList();
+
+        return pageModels.stream()
+            .map(PageEntityMapper::toDomain)
+            .collect(Collectors.toList());
+    }
+
+
+    public static List<PageModel> toModel(List<Page> pages) {
+        if (pages == null) return Collections.emptyList();
+
+        return pages.stream()
+            .map(PageEntityMapper::toModel)
+            .collect(Collectors.toList());
+    }
 }
