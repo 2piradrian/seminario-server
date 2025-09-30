4f4c9210485a99e80596add02ef32be74e784ce3
JulianTs0 <juliantorresssr@gmail.com>
2025-09-30 15:38:43 -0300

    fix: GetPageById response class

--- pages/src/main/java/com/group3/pages/domain/dto/response/GetPageByIdRes.java
+++ pages/src/main/java/com/group3/pages/domain/dto/response/GetPageByIdRes.java
@@ -1,5 +1,6 @@
 package com.group3.pages.domain.dto.response;
 
+import com.group3.entity.PageType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,10 +14,18 @@ public class GetPageByIdRes {
 
     private final String name;
 
-    private final String imageId;
+    private final String portraitImageId;
+
+    private final String profileImageId;
+
+    private final String shortDescription;
+
+    private final String longDescription;
 
     private final String ownerId;
 
     private final List<String> members;
+
+    private PageType pageType;
     
 }
