fea09fccc2d8471c2c3a57fa658d0fb48cd09553
JulianTs0 <juliantorresssr@gmail.com>
2025-09-30 09:40:47 -0300

    fix: rename postgresRepositoryMethod

--- pages/src/main/java/com/group3/pages/data/postgres/repository/PostgresPagesRepositoryI.java
+++ pages/src/main/java/com/group3/pages/data/postgres/repository/PostgresPagesRepositoryI.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 public interface PostgresPagesRepositoryI extends JpaRepository<PageModel, String> {
     
-    Optional<PageModel> findByOwnerId(String ownerId);
+    Optional<PageModel> findByParticipantId(String participantId);
     
     @Query("""
         SELECT p
