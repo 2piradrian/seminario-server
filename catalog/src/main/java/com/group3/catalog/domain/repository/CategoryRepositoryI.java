466f4a91cc5c370e063dc81aa1d8a201b8c89986
DisenRumble <juanpabloquintana2005@gmail.com>
2025-09-30 19:39:24 -0300

    feat

--- catalog/src/main/java/com/group3/catalog/domain/repository/CategoryRepositoryI.java
+++ catalog/src/main/java/com/group3/catalog/domain/repository/CategoryRepositoryI.java
@@ -1,4 +1,17 @@
 package com.group3.catalog.domain.repository;
 
+import com.group3.entity.Category;
+
+import java.util.List;
+
 public interface CategoryRepositoryI {
+
+    Category getById(String categoryId);
+
+    List<Category> getAll();
+
+    Category save(Category category);
+
+    Category update(Category category);
+
 }
