48961344d2a4e2ef1250eadb608220769f7771d5
JulianTs0 <juliantorresssr@gmail.com>
2025-09-30 10:04:22 -0300

    add: Lombok anotations

--- pages/src/main/java/com/group3/pages/domain/dto/response/GetPageByUserId.java
+++ pages/src/main/java/com/group3/pages/domain/dto/response/GetPageByUserId.java
@@ -1,9 +1,13 @@
 package com.group3.pages.domain.dto.response;
 
 import com.group3.entity.Page;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
 public class GetPageByUserId {
 
     List<Page> pages;
