596232982994c176fa7c60bc1cb47902cf40c286
DisenRumble <juanpabloquintana2005@gmail.com>
2025-09-30 18:34:02 -0300

    feat PageType Response

---
+++ catalog/src/main/java/com/group3/catalog/domain/dto/pagetype/response/GetPageTypeListByIdRes.java
@@ -0,0 +1,15 @@
+package com.group3.catalog.domain.dto.pagetype.response;
+
+import com.group3.entity.PageType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class GetPageTypeListByIdRes {
+
+    List<PageType> pageTypes;
+
+}
