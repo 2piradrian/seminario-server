bcc55cc0169bbc0fe21f4411266101522f0897ab
JulianTs0 <juliantorresssr@gmail.com>
2025-09-30 15:42:18 -0300

    fix: EditMapper class

--- pages/src/main/java/com/group3/pages/domain/dto/mapper/implementation/EditMapper.java
+++ pages/src/main/java/com/group3/pages/domain/dto/mapper/implementation/EditMapper.java
@@ -2,6 +2,7 @@ package com.group3.pages.domain.dto.mapper.implementation;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.group3.entity.PageType;
 import com.group3.pages.domain.dto.request.EditPageReq;
 
 import java.util.List;
@@ -15,9 +16,13 @@ public class EditMapper {
         return EditPageReq.create(
             token,
             (String) payload.get("name"),
-            (String) payload.get("base64Image"),
+            (String) payload.get("portraitImage"),
+            (String) payload.get("profileImage"),
+            (String) payload.get("shortDescription"),
+            (String) payload.get("longDescription"),
             (String) payload.get("ownerId"),
-            objectMapper.convertValue(payload.get("members"), new TypeReference<List<String>>() {})
+            objectMapper.convertValue(payload.get("members"), new TypeReference<List<String>>() {}),
+            objectMapper.convertValue(payload.get("pageType"), new TypeReference<PageType>() {})
         );
     }
     
