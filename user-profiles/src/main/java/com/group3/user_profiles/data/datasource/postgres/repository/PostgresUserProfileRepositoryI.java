cc7ef6a534a9d1e9fcb1126b17a6dd14d5cec18a
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-10-12 18:49:57 -0300

    cleaning repository

--- user-profiles/src/main/java/com/group3/user_profiles/data/datasource/postgres/repository/PostgresUserProfileRepositoryI.java
+++ user-profiles/src/main/java/com/group3/user_profiles/data/datasource/postgres/repository/PostgresUserProfileRepositoryI.java
@@ -1,22 +1,26 @@
 package com.group3.user_profiles.data.datasource.postgres.repository;
 
 import com.group3.entity.Status;
+import com.group3.user_profiles.data.datasource.postgres.model.UserProfileModel;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import com.group3.user_profiles.data.datasource.postgres.model.UserProfileModel;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface PostgresUserProfileRepositoryI extends JpaRepository<UserProfileModel, String> {
 
+    // ======== Search ========
+
     Optional<UserProfileModel> findByEmail(String email);
+
+    List<UserProfileModel> findAllByIdIn(List<String> ids);
+
+
+    // ======== Custom Search ========
 
     @Query("""
         SELECT u
@@ -25,15 +29,18 @@ public interface PostgresUserProfileRepositoryI extends JpaRepository<UserProfil
         AND LOWER(CONCAT(u.name, ' ', u.surname)) LIKE LOWER(CONCAT('%', :fullName, '%'))
     """)
     Page<UserProfileModel> findByFullNameLike(
-        @Param("fullName") String fullName,
-        @Param("status") Status status,
-        Pageable pageable
+            @Param("fullName") String fullName,
+            @Param("status") Status status,
+            Pageable pageable
     );
 
+
+    // ======== (followers / following) ========
+
     @Query(
-       value = "SELECT user_id FROM user_following WHERE following_id = :userId ORDER BY user_id ASC",
-       countQuery = "SELECT COUNT(*) FROM user_following WHERE following_id = :userId",
-       nativeQuery = true
+            value = "SELECT user_id FROM user_following WHERE following_id = :userId ORDER BY user_id ASC",
+            countQuery = "SELECT COUNT(*) FROM user_following WHERE following_id = :userId",
+            nativeQuery = true
     )
     Page<String> findFollowers(
             @Param("userId") String userId,
@@ -41,14 +48,17 @@ public interface PostgresUserProfileRepositoryI extends JpaRepository<UserProfil
     );
 
     @Query(
-       value = "SELECT following_id FROM user_following WHERE user_id = :userId ORDER BY following_id ASC",
-       countQuery = "SELECT COUNT(*) FROM user_following WHERE user_id = :userId",
-       nativeQuery = true
+            value = "SELECT following_id FROM user_following WHERE user_id = :userId ORDER BY following_id ASC",
+            countQuery = "SELECT COUNT(*) FROM user_following WHERE user_id = :userId",
+            nativeQuery = true
     )
     Page<String> findFollowing(
             @Param("userId") String userId,
             Pageable pageable
     );
+
+
+    // ======== Count ========
 
     @Query(
             value = "SELECT COUNT(*) FROM user_following WHERE user_id = :userId",
@@ -61,7 +71,5 @@ public interface PostgresUserProfileRepositoryI extends JpaRepository<UserProfil
             nativeQuery = true
     )
     Integer countFollowers(@Param("userId") String userId);
-
-    List<UserProfileModel> findAllByIdIn(List<String> ids);
 
 }
