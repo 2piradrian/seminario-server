cdc99e5a3223cbcf9778db3bdd495b747683c1ac
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-10-13 17:31:46 -0300

    implementing queries

---
+++ posts/src/main/java/com/group3/posts/domain/dto/post/mapper/implementation/GetFilteredPageMapper.java
@@ -0,0 +1,34 @@
+package com.group3.posts.domain.dto.post.mapper.implementation;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.group3.entity.PageContent;
+import com.group3.entity.Post;
+import com.group3.posts.domain.dto.post.request.GetFilteredPostPageReq;
+import com.group3.posts.domain.dto.post.request.GetPostPageReq;
+import com.group3.posts.domain.dto.post.response.GetFilteredPostPageRes;
+import com.group3.posts.domain.dto.post.response.GetPostPageRes;
+
+import java.util.List;
+import java.util.Map;
+
+public class GetFilteredPageMapper {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public GetFilteredPostPageReq toRequest(Map<String, Object> payload) {
+        return GetFilteredPostPageReq.create(
+            (Integer) payload.get("page"),
+            (Integer) payload.get("size"),
+            objectMapper.convertValue(payload.get("ids"), new TypeReference<List<String>>() {}),
+            (String) payload.get("secret")
+        );
+    }
+
+    public GetFilteredPostPageRes toResponse(PageContent<Post> posts) {
+        return new GetFilteredPostPageRes(
+            posts.getContent(),
+            posts.getNextPage()
+        );
+    }
+}
---
+++ posts/src/main/java/com/group3/posts/domain/dto/post/request/GetFilteredPostPageReq.java
@@ -0,0 +1,56 @@
+package com.group3.posts.domain.dto.post.request;
+
+import com.group3.error.ErrorHandler;
+import com.group3.error.ErrorType;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class GetFilteredPostPageReq {
+
+    private final Integer page;
+
+    private final Integer size;
+
+    private final List<String> ids;
+
+    private final String secret;
+
+    private GetFilteredPostPageReq(Integer page, Integer size, List<String> ids, String secret) {
+        this.page = page;
+        this.size = size;
+        this.ids = ids;
+        this.secret = secret;
+    }
+
+    public static GetFilteredPostPageReq create(Integer page, Integer size, List<String> ids, String secret) {
+
+        if (secret == null || secret.isBlank()) {
+            throw new ErrorHandler(ErrorType.UNAUTHORIZED);
+        }
+
+        if (page == null) {
+            throw new ErrorHandler(ErrorType.MISSING_REQUIRED_FIELDS);
+        }
+
+        if (page < 0) {
+            throw new ErrorHandler(ErrorType.INVALID_FIELDS);
+        }
+
+        if (size == null) {
+            throw new ErrorHandler(ErrorType.MISSING_REQUIRED_FIELDS);
+        }
+
+        if (size < 0) {
+            throw new ErrorHandler(ErrorType.INVALID_FIELDS);
+        }
+
+        if (ids == null){
+            throw new ErrorHandler(ErrorType.MISSING_REQUIRED_FIELDS);
+        }
+
+        return new GetFilteredPostPageReq(page, size, ids, secret);
+    }
+
+}
