5b656c1402d72cd09215a9449fb1e970bbe1f824
JulianTs0 <juliantorresssr@gmail.com>
2025-10-13 18:12:30 -0300

    fix: change firm method

--- results/src/main/java/com/group3/results/domain/repository/ProfileRepositoryI.java
+++ results/src/main/java/com/group3/results/domain/repository/ProfileRepositoryI.java
@@ -3,8 +3,10 @@ package com.group3.results.domain.repository;
 import com.group3.entity.PageContent;
 import com.group3.entity.UserProfile;
 
+import java.util.List;
+
 public interface ProfileRepositoryI {
 
-    PageContent<UserProfile> getByFullName(String fullname, Integer page, Integer size);
+    List<UserProfile> getByFullName(String fullname, Integer page, Integer size);
 
 }
