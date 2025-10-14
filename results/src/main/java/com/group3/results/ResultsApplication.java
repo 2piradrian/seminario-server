fee9f73c6a0c6dff98d6d30e81dc2659e4b8e85f
JulianTs0 <juliantorresssr@gmail.com>
2025-10-14 17:26:20 -0300

    add: feignClient annotation

--- results/src/main/java/com/group3/results/ResultsApplication.java
+++ results/src/main/java/com/group3/results/ResultsApplication.java
@@ -2,8 +2,10 @@ package com.group3.results;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ResultsApplication {
 
 	public static void main(String[] args) {
