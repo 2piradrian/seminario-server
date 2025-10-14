fafcdfd3565118b29c92d28b5c500751bc6dff8c
JulianTs0 <juliantorresssr@gmail.com>
2025-10-14 17:21:07 -0300

    add: filtered query

--- page-profiles/src/main/java/com/group3/page_profiles/data/datasource/postgres/repository/PostgresPageProfileRepositoryI.java
+++ page-profiles/src/main/java/com/group3/page_profiles/data/datasource/postgres/repository/PostgresPageProfileRepositoryI.java
@@ -1,6 +1,9 @@
 package com.group3.page_profiles.data.datasource.postgres.repository;
 
+import com.group3.entity.Status;
 import com.group3.page_profiles.data.datasource.postgres.model.PageProfileModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,15 +19,24 @@ public interface PostgresPageProfileRepositoryI extends JpaRepository<PageProfil
     List<PageProfileModel> findAllByIdIn(List<String> ids);
 
 
-    // ======== Custom Search ========
+    // ======== Get filtered By Filtered Page ========
 
     @Query("""
-        SELECT p
-        FROM PageProfileModel p
-        WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))
+        SELECT p FROM PageProfileModel p WHERE
+        p.status = :status AND
+        (:name IS NULL OR LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))) AND
+        (:pageTypeId IS NULL OR p.pageTypeId = :pageTypeId) AND
+        (:#{#memberIds == null or #memberIds.isEmpty()} = true OR p.id IN (
+            SELECT p_m.id FROM PageProfileModel p_m JOIN p_m.members m WHERE m IN :memberIds
+        ))
     """)
-    List<PageProfileModel> findByNameLike(@Param("name") String name);
-
+    Page<PageProfileModel> findByFilteredPage(
+        @Param("name") String name,
+        @Param("status") Status status,
+        @Param("pageTypeId") String pageTypeId,
+        @Param("memberIds") List<String> memberIds,
+        Pageable pageable
+    );
 
     // ======== User Members ========
 
