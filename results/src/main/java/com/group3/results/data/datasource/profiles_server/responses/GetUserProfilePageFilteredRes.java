6a71db63192a86d33e41d2dcf440f5526c351f40
JulianTs0 <juliantorresssr@gmail.com>
2025-10-14 15:29:00 -0300

    fix: add secret validation to active dto

--- results/src/main/java/com/group3/results/data/datasource/profiles_server/responses/GetUserProfilePageFilteredRes.java
+++ results/src/main/java/com/group3/results/data/datasource/profiles_server/responses/GetUserProfilePageFilteredRes.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Getter
 @AllArgsConstructor
-public class GetUserProfilePageByFullnameRes {
+public class GetUserProfilePageFilteredRes {
 
     private final List<UserProfile> profiles;
 
--- user-profiles/src/main/java/com/group3/user_profiles/domain/dto/profile/mapper/implementation/ActiveMapper.java
+++ user-profiles/src/main/java/com/group3/user_profiles/domain/dto/profile/mapper/implementation/ActiveMapper.java
@@ -2,10 +2,15 @@ package com.group3.user_profiles.domain.dto.profile.mapper.implementation;
 
 import com.group3.user_profiles.domain.dto.profile.request.ActiveUserProfileReq;
 
+import java.util.Map;
+
 public class ActiveMapper {
 
-    public ActiveUserProfileReq toRequest(String userId){
-        return ActiveUserProfileReq.create(userId);
+    public ActiveUserProfileReq toRequest(Map<String, Object> payload){
+        return ActiveUserProfileReq.create(
+            (String) payload.get("userId"),
+            (String) payload.get("secret")
+        );
     }
 
 }
