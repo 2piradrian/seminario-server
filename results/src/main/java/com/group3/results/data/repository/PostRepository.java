a2ebe6b2c492774b4ca38d6b4b11dabb46c33493
JulianTs0 <juliantorresssr@gmail.com>
2025-10-17 15:25:21 -0300

    add: add authorization comprobation

--- results/src/main/java/com/group3/results/data/repository/PostRepository.java
+++ results/src/main/java/com/group3/results/data/repository/PostRepository.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.group3.entity.PageContent;
 import com.group3.entity.Post;
 import com.group3.entity.Status;
+import com.group3.entity.User;
+import com.group3.error.ErrorHandler;
+import com.group3.error.ErrorType;
 import com.group3.results.data.datasource.pages_server.repository.PageProfilesServerRepositoryI;
 import com.group3.results.data.datasource.pages_server.responses.GetPageProfilePageFilteredRes;
 import com.group3.results.data.datasource.post_server.repository.PostServerRepositoryI;
@@ -15,18 +18,21 @@ import org.springframework.stereotype.Repository;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Repository
 @AllArgsConstructor
 public class PostRepository implements PostRepositoryI {
 
     private final PostServerRepositoryI repository;
+    private final UserRepository userRepository;
 
     @Override
     public List<Post> getFilteredPosts(String token, List<String> ids, Integer page, Integer size, String text, String secret) {
-        Map<String,Object> payload = new HashMap<>();
 
+        User user = this.userRepository.auth(token);
+        if (user == null) throw new ErrorHandler(ErrorType.USER_NOT_FOUND);
+
+        Map<String,Object> payload = new HashMap<>();
 
         payload.put("page", page);
         payload.put("size", size);
