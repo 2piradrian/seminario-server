34f7d6dcf7dcbf135ca4d52cee965780ab2ff3c8
JulianTs0 <juliantorresssr@gmail.com>
2025-10-18 23:25:56 -0300

    fix: get filtered service

--- posts/src/main/java/com/group3/posts/presentation/service/PostService.java
+++ posts/src/main/java/com/group3/posts/presentation/service/PostService.java
@@ -132,7 +132,7 @@ public class PostService implements PostServiceI {
     @Override
     public GetFilteredPostPageRes getFilteredPosts(GetFilteredPostPageReq dto) {
         if (!this.secretKeyHelper.isValid(dto.getSecret())) throw new ErrorHandler(ErrorType.UNAUTHORIZED);
-        PageContent<Post> posts = this.postsRepository.getFilteredPosts(dto.getIds(), dto.getPage(), dto.getSize(), dto.getText());
+        PageContent<Post> posts = this.postsRepository.getFilteredPosts(dto.getPage(), dto.getSize(), dto.getText());
 
         return PostMapper.getFilteredPage().toResponse(posts);
     }
