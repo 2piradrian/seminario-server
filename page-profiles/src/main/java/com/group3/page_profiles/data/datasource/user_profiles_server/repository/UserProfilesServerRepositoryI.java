c6380267513895aad096b964ba379d32f1169418
JulianTs0 <juliantorresssr@gmail.com>
2025-10-20 20:37:29 -0300

    add: getUserProfileWithFolowers request feing

--- page-profiles/src/main/java/com/group3/page_profiles/data/datasource/user_profiles_server/repository/UserProfilesServerRepositoryI.java
+++ page-profiles/src/main/java/com/group3/page_profiles/data/datasource/user_profiles_server/repository/UserProfilesServerRepositoryI.java
@@ -3,6 +3,7 @@ package com.group3.page_profiles.data.datasource.user_profiles_server.repository
 import com.group3.page_profiles.config.beans.LoadBalancerConfiguration;
 import com.group3.page_profiles.data.datasource.user_profiles_server.responses.GetFollowersByIdRes;
 import com.group3.page_profiles.data.datasource.user_profiles_server.responses.GetUserProfileByIdRes;
+import com.group3.page_profiles.data.datasource.user_profiles_server.responses.GetUserProfileWithFollowingByIdRes;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
@@ -18,5 +19,8 @@ public interface UserProfilesServerRepositoryI {
 
     @PostMapping("/api/user-profiles/get-followers-by-id")
     GetFollowersByIdRes getFollowersById(@RequestBody Map<String, Object> payload);
+
+    @PostMapping("/api/user-profiles/get-by-id-with-following/{userId}")
+    GetUserProfileWithFollowingByIdRes getByIdWithFollowing(@PathVariable(value = "userId") String userId, @RequestBody Map<String, Object> payload);
 
 }
--- page-profiles/src/main/java/com/group3/page_profiles/data/repository/UserProfileRepository.java
+++ page-profiles/src/main/java/com/group3/page_profiles/data/repository/UserProfileRepository.java
@@ -5,6 +5,7 @@ import com.group3.error.ErrorHandler;
 import com.group3.error.ErrorType;
 import com.group3.page_profiles.data.datasource.user_profiles_server.repository.UserProfilesServerRepositoryI;
 import com.group3.page_profiles.data.datasource.user_profiles_server.responses.GetUserProfileByIdRes;
+import com.group3.page_profiles.data.datasource.user_profiles_server.responses.GetUserProfileWithFollowingByIdRes;
 import com.group3.page_profiles.domain.repository.ProfileRepositoryI;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -55,6 +56,33 @@ public class UserProfileRepository implements ProfileRepositoryI {
         payload.put("secret", secret);
 
         return this.repository.getFollowersById(payload).getFollowersCount();
+    }
+
+    // ======== Get Followers By Id ========
+
+    @Override
+    public UserProfile getByIdWithFollowers(String id, String secret) {
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("id", id);
+        payload.put("secret", secret);
+
+        GetUserProfileWithFollowingByIdRes response = this.repository.getByIdWithFollowing(id, payload);
+
+        if (response == null) {
+            throw new ErrorHandler(ErrorType.USER_NOT_FOUND);
+        }
+
+        UserProfile user = new UserProfile();
+        user.setId(response.getId());
+        user.setEmail(response.getEmail());
+        user.setName(response.getName());
+        user.setSurname(response.getSurname());
+        user.setMemberSince(response.getMemberSince());
+        user.setStyles(response.getStyles());
+        user.setInstruments(response.getInstruments());
+        user.setFollowing(response.getFollowing());
+
+        return user;
     }
 
 }
