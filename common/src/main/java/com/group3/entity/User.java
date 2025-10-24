dab20f0a4aa7b8b6659aa8ed70ac8b756db052d2
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-10-24 13:44:09 -0300

    common review

--- common/src/main/java/com/group3/entity/User.java
+++ common/src/main/java/com/group3/entity/User.java
@@ -23,48 +23,25 @@ public class User {
 
     private boolean isPageMember(PageProfile pageProfile) {
         return pageProfile != null
-            && pageProfile.getMembers() != null
-            && pageProfile.getMembers().stream()
-            .anyMatch(member -> member.getId().equals(this.getId()));
+                && pageProfile.getMembers() != null
+                && pageProfile.getMembers().stream()
+                .anyMatch(member -> member.getId().equals(this.getId()));
     }
 
     public boolean canDelete(Post post){
-
-        if(this.rol.canDelete()){
-            return true;
-        }
-
-        if(post.getAuthor() != null && post.getAuthor().getId().equals(this.getId())){
-            return true;
-        }
-
-        PageProfile pageProfile = post.getPageProfile();
-
-        return this.isPageMember(pageProfile);
+        return this.rol.canDelete() ||
+        (post.getAuthor() != null && post.getAuthor().getId().equals(this.getId())) ||
+        this.isPageMember(post.getPageProfile());
     }
 
     public boolean canDelete(PageProfile page){
-
-        if(this.rol.canDelete()){
-            return true;
-        }
-
-        return page.getOwner()!= null && page.getOwner().getId().equals(this.getId());
+        return this.rol.canDelete() || (page.getOwner()!= null && page.getOwner().getId().equals(this.getId()));
     }
 
     public boolean canDelete(Comment comment){
-
-        if(this.rol.canDelete()){
-            return true;
-        }
-
-        if(comment.getAuthor() != null && comment.getAuthor().getId().equals(this.getId())){
-            return true;
-        }
-
-        PageProfile pageProfile = comment.getPageProfile();
-
-        return this.isPageMember(pageProfile);
+        return this.rol.canDelete() ||
+                (comment.getAuthor() != null && comment.getAuthor().getId().equals(this.getId())) ||
+                this.isPageMember(comment.getPageProfile());
     }
 
     /*public boolean canDelete(Event event){
