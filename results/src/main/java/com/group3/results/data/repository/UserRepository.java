83e5155f12eea187394a425dcc4ca4ece0628ab0
JulianTs0 <juliantorresssr@gmail.com>
2025-10-24 12:48:24 -0300

    fix: user feing client with role

--- results/src/main/java/com/group3/results/data/repository/UserRepository.java
+++ results/src/main/java/com/group3/results/data/repository/UserRepository.java
@@ -22,7 +22,7 @@ public class UserRepository implements UserRepositoryI {
         User user = new User();
         user.setId(response.getId());
         user.setEmail(response.getEmail());
-        user.setRoles(response.getRoles());
+        user.setRol(response.getRol());
         user.setStatus(response.getStatus());
 
         return user;
