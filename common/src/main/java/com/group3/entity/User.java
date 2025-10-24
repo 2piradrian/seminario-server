b0f7f4441c12cdcef51181f8b98f6aaf7ab14202
JulianTs0 <juliantorresssr@gmail.com>
2025-10-24 12:35:48 -0300

    add: domain methods to user class

--- common/src/main/java/com/group3/entity/User.java
+++ common/src/main/java/com/group3/entity/User.java
@@ -5,9 +5,6 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @Data
 @Builder
 @NoArgsConstructor
@@ -22,6 +19,67 @@ public class User {
 
     private Status status;
 
-    private List<Role> roles;
+    private Role rol;
+
+    private boolean isPageMember(PageProfile pageProfile) {
+        return pageProfile != null
+            && pageProfile.getMembers() != null
+            && pageProfile.getMembers().stream()
+            .anyMatch(member -> member.getId().equals(this.getId()));
+    }
+
+    public boolean canDelete(Post post){
+
+        if(this.rol.canDelete()){
+            return true;
+        }
+
+        if(post.getAuthor() != null && post.getAuthor().getId().equals(this.getId())){
+            return true;
+        }
+
+        PageProfile pageProfile = post.getPageProfile();
+
+        return this.isPageMember(pageProfile);
+    }
+
+    public boolean canDelete(PageProfile page){
+
+        if(this.rol.canDelete()){
+            return true;
+        }
+
+        return page.getOwner()!= null && page.getOwner().getId().equals(this.getId());
+    }
+
+    public boolean canDelete(Comment comment){
+
+        if(this.rol.canDelete()){
+            return true;
+        }
+
+        if(comment.getAuthor() != null && comment.getAuthor().getId().equals(this.getId())){
+            return true;
+        }
+
+        PageProfile pageProfile = comment.getPageProfile();
+
+        return this.isPageMember(pageProfile);
+    }
+
+    /*public boolean canDelete(Event event){
+
+        if(this.rol.canDelete()){
+            return true;
+        }
+
+        if(event.getAuthor() != null && event.getAuthor().getId().equals(this.getId())){
+            return true;
+        }
+
+        PageProfile pageProfile = event.getPageProfile();
+
+        return this.isPageMember(pageProfile);
+    }*/
 
 }
