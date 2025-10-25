c881ae47d67ad5fe6fa3d44227611440f69bb904
JulianTs0 <juliantorresssr@gmail.com>
2025-10-25 17:00:37 -0300

    add: new findWithExcluded query

--- users/src/main/java/com/group3/users/data/datasource/postgres/repository/PostgresUserRepositoryI.java
+++ users/src/main/java/com/group3/users/data/datasource/postgres/repository/PostgresUserRepositoryI.java
@@ -1,12 +1,24 @@
 package com.group3.users.data.datasource.postgres.repository;
 
+import com.group3.entity.Role;
 import com.group3.users.data.datasource.postgres.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PostgresUserRepositoryI extends JpaRepository<UserModel, String> {
 
     Optional<UserModel> findByEmail(String email);
+
+    @Query("""
+        SELECT u FROM UserModel u 
+        WHERE u.role <> :roleExcluded
+    """)
+    List<UserModel> findWithExcludedRole(
+        @Param("roleExcluded") Role roleExcluded
+    );
 
 }
