914d655da9ade3ea2f438567662e2a63cdeac011
JulianTs0 <juliantorresssr@gmail.com>
2025-10-25 19:03:45 -0300

    add: getById feing method

---
+++ users/src/main/java/com/group3/users/data/datasource/user_profiles_server/responses/GetUserProfileByIdRes.java
@@ -0,0 +1,45 @@
+package com.group3.users.data.datasource.user_profiles_server.responses;
+
+import com.group3.entity.Instrument;
+import com.group3.entity.Style;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class GetUserProfileByIdRes {
+
+    private final String id;
+
+    private final String name;
+
+    private final String surname;
+
+    private final String email;
+
+    private final LocalDateTime memberSince;
+
+    private final String portraitImage;
+
+    private final String profileImage;
+
+    private final String shortDescription;
+
+    private final String longDescription;
+
+    private final List<Style> styles;
+
+    private final List<Instrument> instruments;
+
+    private final Integer followersCount;
+
+    private final Integer followingCount;
+
+    private final Boolean ownProfile;
+
+    private final Boolean isFollowing;
+
+}
