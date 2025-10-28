6a19643d2e0815738132f894b9af3e1a01c3d550
JulianTs0 <juliantorresssr@gmail.com>
2025-10-28 10:44:49 -0300

    add: findBy author & asists querys

--- events/src/main/java/com/group3/events/data/datasource/postgres/repository/PostgresEventRepositoryI.java
+++ events/src/main/java/com/group3/events/data/datasource/postgres/repository/PostgresEventRepositoryI.java
@@ -1,7 +1,42 @@
 package com.group3.events.data.datasource.postgres.repository;
 
+import com.group3.entity.Status;
 import com.group3.events.data.datasource.postgres.model.EventModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostgresEventRepositoryI extends JpaRepository<EventModel, String> {
+
+    // ======== Get Events by Author ========
+
+    @Query("""
+        SELECT e
+        FROM EventModel e
+        WHERE e.authorId = :authorId
+        AND e.status = :status
+        ORDER BY e.createdAt DESC
+    """)
+    Page<EventModel> findByAuthorIdAndStatus(
+            @Param("authorId") String authorId,
+            @Param("status") Status status,
+            Pageable pageable
+    );
+
+    // ======== Get Events by Assistant and Status ========
+
+    @Query("""
+        SELECT e
+        FROM EventModel e
+        WHERE :userId MEMBER OF e.assist
+        AND e.status = :status
+        ORDER BY e.createdAt DESC
+    """)
+    Page<EventModel> findByAssistContainsAndStatus(
+            @Param("userId") String userId,
+            @Param("status") Status status,
+            Pageable pageable
+    );
 }
