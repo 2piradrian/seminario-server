9fa06e1f111e4a28a9b7db4787975199666040a4
JulianTs0 <juliantorresssr@gmail.com>
2025-10-29 09:16:39 -0300

    fix: events feing client

--- events/src/main/java/com/group3/events/data/repository/UserRepository.java
+++ events/src/main/java/com/group3/events/data/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.group3.events.data.repository;
 
 import com.group3.entity.User;
+import com.group3.entity.UserProfile;
+import com.group3.error.ErrorHandler;
+import com.group3.error.ErrorType;
 import com.group3.events.data.datasource.users_server.repository.UsersServerRepositoryI;
 import com.group3.events.data.datasource.users_server.responses.AuthUserRes;
 import com.group3.events.data.datasource.users_server.responses.GetUserByIdRes;
@@ -24,8 +27,21 @@ public class UserRepository implements UserRepositoryI {
         User user = new User();
         user.setId(response.getId());
         user.setEmail(response.getEmail());
-        user.setRole(response.getRol());
+        user.setRole(response.getRole());
         user.setStatus(response.getStatus());
+
+        UserProfile profile = new UserProfile();
+        profile.setName(response.getProfile().getName());
+        profile.setSurname(response.getProfile().getSurname());
+        profile.setMemberSince(response.getProfile().getMemberSince());
+        profile.setPortraitImage(response.getProfile().getPortraitImage());
+        profile.setProfileImage(response.getProfile().getProfileImage());
+        profile.setShortDescription(response.getProfile().getShortDescription());
+        profile.setLongDescription(response.getProfile().getLongDescription());
+        profile.setStyles(response.getProfile().getStyles());
+        profile.setInstruments(response.getProfile().getInstruments());
+
+        user.setProfile(profile);
 
         return user;
     }
@@ -34,14 +50,31 @@ public class UserRepository implements UserRepositoryI {
     // ======== Single User Retrieval ========
 
     @Override
-    public User getById(String userId) {
-        GetUserByIdRes response = this.repository.getById(userId);
+    public User getById(String userId, String token) {
+        GetUserByIdRes response = this.repository.getById(token, userId);
+
+        if (response == null) {
+            throw new ErrorHandler(ErrorType.USER_NOT_FOUND);
+        }
 
         User user = new User();
         user.setId(response.getId());
         user.setEmail(response.getEmail());
-        user.setRole(response.getRol());
+        user.setRole(response.getRole());
         user.setStatus(response.getStatus());
+
+        UserProfile profile = new UserProfile();
+        profile.setName(response.getProfile().getName());
+        profile.setSurname(response.getProfile().getSurname());
+        profile.setMemberSince(response.getProfile().getMemberSince());
+        profile.setPortraitImage(response.getProfile().getPortraitImage());
+        profile.setProfileImage(response.getProfile().getProfileImage());
+        profile.setShortDescription(response.getProfile().getShortDescription());
+        profile.setLongDescription(response.getProfile().getLongDescription());
+        profile.setStyles(response.getProfile().getStyles());
+        profile.setInstruments(response.getProfile().getInstruments());
+
+        user.setProfile(profile);
 
         return user;
     }
