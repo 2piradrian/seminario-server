d16d7bfe5fd08734cd3e3cf083e5c1208cdbb048
DisenRumble <juanpabloquintana2005@gmail.com>
2025-10-29 13:21:17 -0300

    fix ¡

--- users/src/main/java/com/group3/users/domain/validator/RegexValidators.java
+++ users/src/main/java/com/group3/users/domain/validator/RegexValidators.java
@@ -9,8 +9,8 @@ public enum RegexValidators {
     SURNAME("^[\\p{L}]+(?:[ '-][\\p{L}]+)*$"),
     PASSWORD("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)\\S{8,}$"),
     EMAIL("^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$"),
-    SHORT_DESCRIPTION("^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9.,!?()'\"\\-\\s]{0,50}$"),
-    LONG_DESCRIPTION("^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9.,!?()'\"\\-\\s]{0,150}$"),
+    SHORT_DESCRIPTION("^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9.,!¡?()'\"\\-\\s]{0,50}$"),
+    LONG_DESCRIPTION("^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9.,!¡?()'\"\\-\\s]{0,150}$"),
     IMAGE_URL("^https?://[a-zA-Z0-9\\-\\.]+\\.[a-zA-Z]{2,}[^\\s]*$");
 
     private final String regex;
