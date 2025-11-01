db165300fc34148bde15673308c09a792a1b1384
JulianTs0 <juliantorresssr@gmail.com>
2025-11-01 18:11:24 -0300

    fix: join events gets repositories

--- events/src/main/java/com/group3/events/domain/repository/EventRepositoryI.java
+++ events/src/main/java/com/group3/events/domain/repository/EventRepositoryI.java
@@ -11,8 +11,5 @@ public interface EventRepositoryI {
 
     Event update(Event event);
 
-    PageContent<Event> getPageEventsByAuthorId(String userId, Integer page, Integer size);
-
-    PageContent<Event> getPageEventsByAssistant(String userId, Integer page, Integer size);
-
+    PageContent<Event> getByAuthorOrAssistant(String authorId, Integer page, Integer size);
 }
