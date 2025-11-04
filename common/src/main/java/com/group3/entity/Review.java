7c99c4f227af94576d57c33f8834bccc5317007d
JulianTs0 <juliantorresssr@gmail.com>
2025-11-04 10:55:07 -0300

    fix: review entity

--- common/src/main/java/com/group3/entity/Review.java
+++ common/src/main/java/com/group3/entity/Review.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -20,5 +22,11 @@ public class Review {
     private String review;
 
     private Float rating;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private Status status;
 
 }
