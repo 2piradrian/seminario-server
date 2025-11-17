509562212231821b6f424f82c1adc9b6862c7c6c
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-11-17 17:19:55 -0300

    notification repositories

--- notifications/src/main/java/com/group3/notifications/domain/repository/UserRepositoryI.java
+++ notifications/src/main/java/com/group3/notifications/domain/repository/UserRepositoryI.java
@@ -1,9 +1,6 @@
 package com.group3.notifications.domain.repository;
 
-import com.group3.entity.Follow;
 import com.group3.entity.User;
-
-import java.util.List;
 
 public interface UserRepositoryI {
 
