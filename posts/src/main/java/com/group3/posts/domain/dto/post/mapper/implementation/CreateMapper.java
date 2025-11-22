bd925ecfb315ec877602c886896625aa058cdbee
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-11-21 21:32:19 -0300

    creating post

--- posts/src/main/java/com/group3/posts/domain/dto/post/mapper/implementation/CreateMapper.java
+++ posts/src/main/java/com/group3/posts/domain/dto/post/mapper/implementation/CreateMapper.java
@@ -1,18 +1,13 @@
 package com.group3.posts.domain.dto.post.mapper.implementation;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.group3.entity.PageType;
 import com.group3.entity.Post;
-import com.group3.entity.PostType;
 import com.group3.posts.domain.dto.post.request.CreatePostReq;
 import com.group3.posts.domain.dto.post.response.CreatePostRes;
 
 import java.util.Map;
 
 public class CreateMapper {
-
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
     public CreatePostReq toRequest(String token, Map<String, Object> payload) {
 
@@ -22,7 +17,7 @@ public class CreateMapper {
             (String) payload.get("content"),
             (String) payload.get("profileId"),
             (String) payload.get("image"),
-            objectMapper.convertValue(payload.get("postType"), new TypeReference<PostType>(){})
+            (String) payload.get("postTypeId")
         );
     }
 
