b3d3ed985c7f001fbf77e36cd90c912c2885818e
JulianTs0 <juliantorresssr@gmail.com>
2025-11-22 00:54:54 -0300

    fix: post get filtered

--- posts/src/main/java/com/group3/posts/presentation/service/PostService.java
+++ posts/src/main/java/com/group3/posts/presentation/service/PostService.java
@@ -144,6 +144,9 @@ public class PostService implements PostServiceI {
 
     @Override
     public GetFilteredPostPageRes getFilteredPosts(GetFilteredPostPageReq dto) {
+        User user = userRepository.auth(dto.getToken());
+        if (user == null) throw new ErrorHandler(ErrorType.UNAUTHORIZED);
+
         if (!this.secretKeyHelper.isValid(dto.getSecret())) throw new ErrorHandler(ErrorType.UNAUTHORIZED);
         PageContent<Post> posts = this.postsRepository.getFilteredPosts(dto.getPage(), dto.getSize(), dto.getText(), dto.getPostTypeId());
 
