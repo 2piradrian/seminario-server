fe70dae69ba51937cbd449fe3a2bc2a8c9aecf16
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-11-30 13:03:03 -0300

    chat data layer

---
+++ chat/src/main/java/com/group3/chat/data/datasource/users_server/repository/UsersServerRepositoryI.java
@@ -0,0 +1,27 @@
+package com.group3.chat.data.datasource.users_server.repository;
+
+import com.group3.chat.config.beans.LoadBalancerConfiguration;
+import com.group3.chat.data.datasource.users_server.responses.AuthUserRes;
+import com.group3.chat.data.datasource.users_server.responses.GetUserByIdRes;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+@FeignClient(name = "users-server", path = "/users-server")
+@LoadBalancerClient(name = "users-server", configuration = LoadBalancerConfiguration.class)
+public interface UsersServerRepositoryI {
+
+    @GetMapping("/api/auth")
+    AuthUserRes auth(
+            @RequestHeader(value = "Authorization") String token
+    );
+
+    @GetMapping("/api/users/get-by-id/{userId}")
+    GetUserByIdRes getById(
+            @RequestHeader(value = "Authorization") String token,
+            @PathVariable(value = "userId") String userId
+    );
+
+}
