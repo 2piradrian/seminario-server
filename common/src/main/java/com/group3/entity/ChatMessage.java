26f4f9f736e585eb697ee1bdeff0a7954bb76893
Adrian Rodriguez <rodriguezcadr@gmail.com>
2025-11-30 12:33:36 -0300

    chat config

---
+++ common/src/main/java/com/group3/entity/ChatMessage.java
@@ -0,0 +1,28 @@
+package com.group3.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+
+    private String id;
+
+    private String targetId;
+
+    private String sourceId;
+
+    private NotificationContent content;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+}
