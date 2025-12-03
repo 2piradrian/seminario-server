599a3c61618a594a7792e9e95aa3a2fe4967457a
JulianTs0 <juliantorresssr@gmail.com>
2025-12-03 17:20:26 -0300

    add: getByRange dto

---
+++ events/src/main/java/com/group3/events/domain/dto/event/response/GetEventByDateRangeRes.java
@@ -0,0 +1,15 @@
+package com.group3.events.domain.dto.event.response;
+
+import com.group3.entity.Event;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class GetEventByDateRangeRes {
+
+    private final List<Event> events;
+
+}
