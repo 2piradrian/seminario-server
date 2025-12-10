664c2a5ea9c98058f3313ba3a3d6676bd54706bd
JulianTs0 <juliantorresssr@gmail.com>
2025-12-10 12:45:16 -0300

    add: getMergedFeed controller

--- results/src/main/java/com/group3/results/presentation/controller/ResultController.java
+++ results/src/main/java/com/group3/results/presentation/controller/ResultController.java
@@ -1,6 +1,7 @@
 package com.group3.results.presentation.controller;
 
 import com.group3.results.domain.dto.mapper.ResultsMapper;
+import com.group3.results.domain.dto.request.GetFeedMergedCursorPageReq;
 import com.group3.results.domain.dto.request.GetFeedPageReq;
 import com.group3.results.domain.dto.request.GetSerchResultFilteredReq;
 import com.group3.results.presentation.service.ResultService;
@@ -8,6 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -44,6 +46,17 @@ public class ResultController {
     ) {
         GetFeedPageReq dto = ResultsMapper.getFeed().toRequest(page, size, token);
         return ResponseEntity.ok(this.resultService.getFeedPage(dto));
+    }
+
+    @GetMapping("/get-feed-merged")
+    public ResponseEntity<?> getFeedMerged(
+        @RequestHeader(value = "Authorization") String token,
+        @RequestParam(value = "profileId") String profileId,
+        @RequestParam(value = "cursor", required = false) LocalDateTime cursor,
+        @RequestParam(value = "size") Integer size
+    ) {
+        GetFeedMergedCursorPageReq dto = ResultsMapper.getFeedMerged().toRequest(token, profileId, cursor, size);
+        return ResponseEntity.ok(this.resultService.getMergedFeedPage(dto));
     }
 
 }
