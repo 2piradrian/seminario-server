be6e21f1aaae0c31a6054ab0e2ed49e3dcdcc417
JulianTs0 <juliantorresssr@gmail.com>
2025-12-14 11:23:52 -0300

    add: cancelEvent dtos

---
+++ events/src/main/java/com/group3/events/domain/dto/event/response/CancelEventRes.java
@@ -0,0 +1,12 @@
+package com.group3.events.domain.dto.event.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CancelEventRes {
+
+    private final String eventId;
+
+}
