1f96f1dacb79abc2c2eab8f49cd616972abe348f
JulianTs0 <juliantorresssr@gmail.com>
2026-01-05 10:55:30 -0300

    add: notification delete feing method

--- users/src/main/java/com/group3/users/domain/repository/NotificationsRepositoryI.java
+++ users/src/main/java/com/group3/users/domain/repository/NotificationsRepositoryI.java
@@ -1,5 +1,9 @@
 package com.group3.users.domain.repository;
 
 public interface NotificationsRepositoryI {
+
     void create(String secret, String targetId, String sourceId,String carriedOutById, String content);
+
+    void deleteBySourceId(String token, String secret, String soruceId);
+
 }
