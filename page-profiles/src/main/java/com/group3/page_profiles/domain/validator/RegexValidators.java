29e0aa7e78612825211d3d8e181d6499e5ba865f
JulianTs0 <juliantorresssr@gmail.com>
2026-01-05 11:03:54 -0300

    fix: regex validations, add: regex validations to page

--- page-profiles/src/main/java/com/group3/page_profiles/domain/validator/RegexValidators.java
+++ page-profiles/src/main/java/com/group3/page_profiles/domain/validator/RegexValidators.java
@@ -4,10 +4,10 @@ import lombok.Getter;
 
 @Getter
 public enum RegexValidators {
-    
-    NAME("^[\\p{L}]+(?:[ '-][\\p{L}]+)*$"),
-    SHORT_DESCRIPTION("^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9.,!?'\"¡¿@%&$*+_:;\\-\\s]{1,50}$"),
-    LONG_DESCRIPTION("^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9.,!?'\"¡¿@%&$*+_:;\\-\\s]{1,150}$");
+
+    NAME("^[\\p{L}]{2,20}(?:[ '-][\\p{L}]{2,20})*$"),
+    SHORT_DESCRIPTION("^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9 @!|¿?¡%&$*+_:;=\\/#\\-\\^~`'\",.]{1,50}$"),
+    LONG_DESCRIPTION("^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9 @!|¿?¡%&$*+_:;=\\/#\\-\\^~`'\",.]{1,150}$");
 
     private final String regex;
 
