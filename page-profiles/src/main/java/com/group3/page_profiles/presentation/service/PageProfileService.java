15c26874f78369f6cf37c2303abc8c418e18a6b8
Adrian Rodriguez <rodriguezcadr@gmail.com>
2026-02-12 15:34:27 -0300

    pages delete

--- page-profiles/src/main/java/com/group3/page_profiles/presentation/service/PageProfileService.java
+++ page-profiles/src/main/java/com/group3/page_profiles/presentation/service/PageProfileService.java
@@ -339,6 +339,16 @@ public class PageProfileService implements PageProfileServiceI {
             throw new ErrorHandler(ErrorType.UNAUTHORIZED);
         }
 
+        if (user.isStaff() && !page.getOwner().getId().equals(user.getId())) {
+            this.notificationRepository.create(
+                    this.secretKeyHelper.getSecret(),
+                    page.getOwner().getId(),
+                    page.getId(),
+                    NotificationContent.MODERATION.name(),
+                    dto.getReasonId()
+            );
+        }
+
         if (page.getProfileImage() != null && !page.getProfileImage().isEmpty()) {
             this.imagesRepository.delete(page.getProfileImage(), secretKeyHelper.getSecret());
         }
